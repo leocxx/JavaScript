22a337eb653fbada31c5a0f0e3c2773dcbce85e5
leocxx <ccxx422320@gmail.com>
2023-06-02 23:54:53 +0800

    add code

---
+++ basic-js/javascript/code.js
@@ -0,0 +1,14 @@
+//1.变量
+// var name = "真实";
+// let username = "测试";
+// const name = "asf";
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+let myArr = [1,2,3,4,5,6];
+// console.log(myArr.length);
+
+for (let i = 0; i < myArr.length; i++) {
+       console.log(myArr[i]);
+}
